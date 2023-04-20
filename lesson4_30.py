77104c559e6da6ba51a935d1e4aa739b4a654039
2f <twawfwstp2@gmail.com>
2023-04-21 08:11:39 +0900

    2023-04-20.21_learn

---
+++ lesson4_30.py
@@ -0,0 +1,10 @@
+my_friends = {'A', 'C', 'D'}
+A_friends = {'B', 'D', 'E', 'F'}
+print(my_friends & A_friends)
+
+f = ['apple', 'banana', 'apple', 'banana']
+print(f)
+print(type(f))
+kind = set(f)
+print(kind)
+print(type(kind))
