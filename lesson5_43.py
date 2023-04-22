fcd63fb3f77bcd8f64c657f7a89fd7eb8ab49c01
2f <twawfwstp2@gmail.com>
2023-04-23 00:27:11 +0900

    0422_learn

---
+++ lesson5_43.py
@@ -0,0 +1,8 @@
+for fruit in ['apple', 'banana', 'orange']:
+    if fruit == 'banana':
+        print('stop eating')
+        break
+    print(fruit)
+
+else:
+    print('I ate all!!')
---
+++ lesson5_47.py
@@ -0,0 +1,6 @@
+d = {'x':100, 'y':200}
+
+print(d.items())
+
+for k, v in d.items():
+    print(k, ':', v)
